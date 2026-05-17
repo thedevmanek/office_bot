7c272b3d6f30b6a49a54fc8e7f5273f86cc02fd9
Dev Manek <thedevmanek@gmail.com>
2026-05-17 11:20:24 +0100

    new bring up

--- dev_ws/launch_sim.sh
+++ dev_ws/launch_sim.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 source /opt/ros/humble/setup.zsh
 source install/local_setup.zsh
+OFFICE_BOT_MODEL_SHARE="$(ros2 pkg prefix office_bot_model)/share/office_bot_model"
 export GZ_SIM_SYSTEM_PLUGIN_PATH=/opt/ros/humble/lib/
 export MESA_GL_VERSION_OVERRIDE=3.3
-export GZ_SIM_RESOURCE_PATH=$GAZEBO_MODEL_PATH:/home/thedevmanek/office_bot/dev_ws/src/office_bot_model/models/worlds/office_world/models
+export GZ_SIM_RESOURCE_PATH=$GAZEBO_MODEL_PATH:$OFFICE_BOT_MODEL_SHARE/models/worlds/office_world/models
 ros2 launch office_bot_model launch_sdf_into_gazebo.launch.py
